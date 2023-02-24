43b9a92417baff9063520620c90dcb3faae99348
swatiyad <swati.nadcabtechnology@gmail.com>
2023-02-24 17:54:00 +0530

    ui improvments

--- apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
+++ apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
@@ -2,11 +2,15 @@ import $ from 'jquery'
 import Cookies from 'js-cookie'
 
 $('.dark-mode-changer-btn').on('click', function() {
+    $(this).find('.fa-moon,.fa-sun').toggleClass('fa-moon').toggleClass('fa-sun')
     if (Cookies.get('chakra-ui-color-mode') === 'dark') {
         Cookies.set('chakra-ui-color-mode', 'light')
+       
     } else {
         Cookies.set('chakra-ui-color-mode', 'dark')
+       
     }
     // reload each theme switch
     document.location.reload(true)
 })
+
